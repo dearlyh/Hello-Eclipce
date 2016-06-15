f37254f66c0c1267128fa3a8eb581f5512292d22
Yunhee Lee <yunheelee73@gmail.com>
2016-06-15 14:32:45 +0900

    test4

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");  
 		System.out.println("It is too hard for me");
+		System.out.println("I will be in success");
 	}
 }
