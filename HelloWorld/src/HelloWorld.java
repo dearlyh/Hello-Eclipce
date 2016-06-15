5dd3253c2f15c31daaf38cff789732daa53e165b
Yunhee Lee <yunheelee73@gmail.com>
2016-06-15 15:24:20 +0900

    Branch test

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -4,5 +4,7 @@ public class HelloWorld {
 		System.out.println("Hello World");  
 		System.out.println("It is too hard for me");
 		System.out.println("I will be in success");
+		System.out.println("hahaha");
+		System.out.println("World's End Dancehall");
 	}
 }
