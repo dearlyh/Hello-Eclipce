97bcea1f74fa01fa05d6b027d674c0182a7e5469
Yunhee Lee <yunheelee73@gmail.com>
2016-06-15 15:35:46 +0900

    branch change

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -6,5 +6,6 @@ public class HelloWorld {
 		System.out.println("I will be in success");
 		System.out.println("hahaha");
 		System.out.println("World's End Dancehall");
+		System.out.println("Hello Dance World");
 	}
 }
